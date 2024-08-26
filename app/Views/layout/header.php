35ad5380316933d4502c198b025b1382a187607c
Fernando <vluisfernando62@gmail.com>
2024-08-26 18:27:55 -0400

    feat(layout): add the header, navbar and footer

---
+++ app/Views/layout/header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title><?= $title ?></title>
+    <!-- CSS files -->
+    <link href="../css/tabler.min.css?1692870487" rel="stylesheet" />
+    <link href="../css/tabler-flags.min.css?1692870487" rel="stylesheet" />
+    <link href="../css/tabler-vendors.min.css?1692870487" rel="stylesheet" />
+    <link href="../css/demo.min.css?1692870487" rel="stylesheet" />
+    <link href="../css/my-style.css" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+</head>
+
+<body>
+    <div class="page">
